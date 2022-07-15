70c5aa75375d9dcafc151a334c34845d4d59e11d
Paul Vincent <paulvincent.pejana@gmail.com>
2022-07-15 10:52:31 +0800

    Added a template for the Blog post

---
+++ page-97196.php
@@ -0,0 +1,38 @@
+<?php 
+/* Template Name: Blogs */
+?>
+
+<?php get_header(); ?>
+<section class="entry-content">
+    <div>
+        <?php 
+            $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1 ;
+            $posts = new WP_Query( array(
+                'post_type'   => "post",
+                'posts_per_page' => 10,
+                'orderby' => "date",
+                'order' => "DESC",
+                'paged' => $paged
+            ) );
+            
+            if ( $posts->have_posts() ) {
+                while ( $posts->have_posts() ){
+                    $posts->the_post();
+                    ?>
+                        <h2><a href="<?php the_permalink(); ?>"><?php echo get_the_title(); ?></a></h2>
+                        <span><?php echo get_comments_number(); ?> Replies</span>
+                    <?php                    
+                }
+            }
+
+        ?>
+        <div>
+            <?php next_posts_link( 'Older Posts', $posts->max_num_pages ); ?>
+            <?php previous_posts_link('Newer Posts', $posts->max_num_pages); ?>
+        </div>
+    </div>
+    <div>
+        
+    </div>
+</section> 
+<?php get_footer(); ?>
