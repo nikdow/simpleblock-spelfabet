9dbea49a3c3de7d08668751c519c5bfd2f0e26ed
nikdow <nik@cbdweb.net>
2022-07-15 14:39:16 +1000

    register sidebar in function.php;

--- functions.php
+++ functions.php
@@ -13,3 +13,21 @@ function spelfabet_enqueue_styles() {
 }
 
 add_action( 'wp_enqueue_scripts', 'spelfabet_enqueue_styles' );
+
+/**
+ * Register our sidebars and widgetized areas.
+ *
+ */
+function spelfabet_widgets_init() {
+
+  register_sidebar( array(
+      'name'          => 'Home right sidebar',
+      'id'            => 'home_right_1',
+      'before_widget' => '<div>',
+      'after_widget'  => '</div>',
+      'before_title'  => '<h2 class="rounded">',
+      'after_title'   => '</h2>',
+  ) );
+
+}
+add_action( 'widgets_init', 'spelfabet_widgets_init' );
