8fd53b3d0d5fe21665716958f04508b86691601a
Paul Vincent <paulvincent.pejana@gmail.com>
2022-07-15 13:19:23 +0800

    Updated index and added post styling

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <?php get_header(); ?>
 <section class="entry-content">
-  <?php include_once "sidebar.php"?>
     <div>
         <?php the_content(); ?>
     </div>
     <div>
-        
+      <?php include_once "sidebar.php"?>
     </div>
 </section> 
 <?php get_footer(); ?>
