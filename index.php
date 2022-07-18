9a5f7d3bdfdf39ac378f5d5ddfffc7fe8b48e274
Paul Vincent <paulvincent.pejana@gmail.com>
2022-07-18 15:21:44 +0800

    Updated index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php get_header(); ?>
 <section class="entry-content">
-    <div>
+    <div class="gc-content">
         <?php the_content(); ?>
     </div>
-    <div>
+    <div class="gc-sidebar">
       <?php include_once "sidebar.php"?>
     </div>
 </section> 
