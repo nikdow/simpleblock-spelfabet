4b7c28f70e77ed5e59f56b0198d591eef90b5f35
Paul Vincent <paulvincent.pejana@gmail.com>
2022-07-19 10:25:20 +0800

    Added conditions for category, post, and date archive page

--- index.php
+++ index.php
@@ -1,7 +1,30 @@
 <?php get_header(); ?>
 <section class="entry-content">
     <div class="gc-content">
-        <?php the_content(); ?>
+      <?php 
+          global $wp_query;
+           
+          if ( is_category() || is_archive() || is_home() ){
+            
+            $posts = new WP_Query( $wp_query->query_vars );
+          
+            if ( $posts->have_posts() ) {
+                while ( $posts->have_posts() ){
+                    $posts->the_post();
+                    ?>
+                        <div class="post">
+                            <h2><a href="<?php the_permalink(); ?>"><?php echo get_the_title(); ?></a></h2>
+                            <a href="<?php the_permalink(); ?>#comments"><span><?php echo get_comments_number(); ?> Replies</span></a>
+                            <?php the_post_thumbnail(); ?>
+                            <?php the_content(); ?>
+                        </div>
+                    <?php                    
+                }
+            }
+          } else {
+            the_content(); 
+          }
+      ?>
     </div>
     <div class="gc-sidebar">
       <?php include_once "sidebar.php"?>
